d9da4380af17bc1dbab02e161fef94a0f20d1f7e
Neeemaaa <89361080+Neeemaaa@users.noreply.github.com>
2023-12-22 10:20:03 -0800

    Update ContentView.swift
    Added launch screen

--- Lease Info/ContentView.swift
+++ Lease Info/ContentView.swift
@@ -11,26 +11,44 @@ struct ContentView: View {
     @State private var currentMileage: Double = 0.0
     @State private var selectedMileage: Double = 12000.0 // Default to 12,000
     @State private var downPayment: Double = 0.0
-    
+    @State private var isActive: Bool = false // State for loading animation
+
     var body: some View {
-        TabView {
-            EnterMileage(currentMileage: $currentMileage, selectedMileage: $selectedMileage)
-                .tabItem {
-                    Image(systemName: "plus")
-                    Text("Enter Mileage")
-                }
+        ZStack {
+            if self.isActive {
+                TabView {
+                    EnterMileage(currentMileage: $currentMileage, selectedMileage: $selectedMileage)
+                        .tabItem {
+                            Image(systemName: "plus")
+                            Text("Enter Mileage")
+                        }
 
-            LeaseInfo(currentMileage: currentMileage, selectedMileage: $selectedMileage)
-                .tabItem {
-                    Image(systemName: "car")
-                    Text("Lease Info")
+                    LeaseInfo(currentMileage: currentMileage, selectedMileage: $selectedMileage)
+                        .tabItem {
+                            Image(systemName: "car")
+                            Text("Lease Info")
+                        }
+                    LeaseCalc()
+                        .tabItem {
+                            Image(systemName: "dollarsign.circle")
+                            Text("Lease Calculator")
+                        }
                 }
-            LeaseCalc()
-                .tabItem {
-                    Image(systemName: "dollarsign.circle")
-                    Text("Lease Calculator")
+            } else {
+                Rectangle()
+                    .background(Color.black)
+                Image("m-loading") // Replace with your image
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 600, height: 1100)
+            }
+        }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
+                withAnimation {
+                    self.isActive = true
                 }
-
+            }
         }
     }
 }
